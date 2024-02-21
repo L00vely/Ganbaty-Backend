2c9208bd0f61ee0e9d3478ad0ef062d2e4667ac0
L00vely <peralesrodriguezdavid@gmail.com>
2024-02-20 21:12:03 -0600

    fix: Preparing to EC2 instance

--- application.py
+++ application.py
@@ -3,12 +3,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
 @app.route('/', methods=['GET'])
 def index():
     # Lógica para manejar la solicitud GET a la ruta raíz
-    data = {'message': 'Hello, World!'}
-    return jsonify(data)
+    # data = {'message': 'Hello, World!'}
+    # return jsonify(data)
+    return "Hello, I'm ready to help"
+
+if __name__ == '__main__':
+    app.run()
